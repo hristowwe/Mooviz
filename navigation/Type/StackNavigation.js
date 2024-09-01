ce5f3de1ac36c89a54a45fcb06e690b63e0a693d
hristowwe <hristowwe@abv.bg>
2024-09-01 22:11:49 +0300

    Fix

--- navigation/Type/StackNavigation.js
+++ navigation/Type/StackNavigation.js
@@ -1,25 +1,12 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React, { useEffect } from "react";
+import React from "react";
 
 import { StackNav } from "../NavigationKeys";
 import { StackRoute } from "../NavigationRoutes";
 
-import { useColorScheme } from "react-native";
-import { useDispatch } from "react-redux";
-import { setTheme } from "../../store/themeSlice";
-import { colors } from "../../themes";
-
 const Stack = createNativeStackNavigator();
 
 export default function StackNavigation() {
-  const dispatch = useDispatch();
-  const colorScheme = useColorScheme();
-  useEffect(() => {
-    console.log(colorScheme);
-    const initialTheme = colorScheme === "dark" ? colors.dark : colors.light;
-    dispatch(setTheme(initialTheme));
-    console.log("theme");
-  }, []);
   return (
     <Stack.Navigator
       screenOptions={{
